168505fe261e75f27c6d45960fa5dbe667640d25
Issac Lisbon <izlisbon@microsoft.com>
2023-05-17 23:27:24 -0700

    support dashboard

--- src/App.js
+++ src/App.js
@@ -1,34 +1,65 @@
 import React, { useRef, useEffect } from 'react';
+import { useMsal } from '@azure/msal-react';
+import { InteractionStatus, InteractionRequiredAuthError } from '@azure/msal-browser';
 import './app.css';
 
+const iframeConfig = {
+  cluster: "kwetest.eastus",
+  database: "covid",
+  features: "f-IFrameAuth=true",
+  workspaceName: "kwe-embed-demo"
+}
 
 export function App() {
     const iframeRef = useRef(null);
-
+    const { instance, inProgress, accounts } = useMsal();
+    
     useEffect(() => {
-        const handleIframeMessage = (event) => {
-            console.log("event.data.type: " + event.data.type);
-            const iframe = iframeRef.current;
-            if (event.data.type === "getToken" && iframe && iframe.contentWindow) {
-                debugger;
-                iframe.contentWindow.postMessage({
-                    "type": "postToken",
-                    "message": "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyIsImtpZCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyJ9.eyJhdWQiOiJodHRwczovL2hlbHAua3VzdG8ud2luZG93cy5uZXQiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC83MmY5ODhiZi04NmYxLTQxYWYtOTFhYi0yZDdjZDAxMWRiNDcvIiwiaWF0IjoxNjg0MzY4NDYwLCJuYmYiOjE2ODQzNjg0NjAsImV4cCI6MTY4NDM3Mzc3OCwiYWNyIjoiMSIsImFpbyI6IkFZUUFlLzhUQUFBQVZHQnJlNWFqWEdaRDJhaTNRbG9TZC9iVnkwNzZ3VHVDNEpaNjEzY0hLeEx3Sk5PdkZYY2hhZXh4VzNVbFNQZVE1VEdHVzBPMFlrREZJUzRtY2NsM1N1a1VrUm1GcEdDbjZ4bXpVRzFoeXdVVVBYQU5hU0JldUdSYmhDTVpoelo0NDZ3V1FVK0UyeDZ4cWMyL3pnUVhNeDJJMTk2Y2lid1h0dU5CcHh0emFTUT0iLCJhbXIiOlsicnNhIiwibWZhIl0sImFwcGlkIjoiMDRiMDc3OTUtOGRkYi00NjFhLWJiZWUtMDJmOWUxYmY3YjQ2IiwiYXBwaWRhY3IiOiIwIiwiZGV2aWNlaWQiOiIxZmQwZDA5OS1lNWQxLTQ1NjItODQ1Mi04MDRjYjIzODI3ZTUiLCJmYW1pbHlfbmFtZSI6Ikxpc2JvbiIsImdpdmVuX25hbWUiOiJJc3NhYyIsImlwYWRkciI6IjczLjE2OS4xNTAuMjQ0IiwibmFtZSI6Iklzc2FjIExpc2JvbiIsIm9pZCI6IjNkNTgxMWU2LTI4MTktNGI3Ny05M2RmLTIwYWU5NzZmY2FhNSIsIm9ucHJlbV9zaWQiOiJTLTEtNS0yMS0yMTI3NTIxMTg0LTE2MDQwMTI5MjAtMTg4NzkyNzUyNy00MjA4NzAzNSIsInB1aWQiOiIxMDAzMjAwMEFCNTUzNDA1IiwicmgiOiIwLkFRRUF2NGo1Y3ZHR3IwR1JxeTE4MEJIYlIzZnFSaWNDUjBWTGdNbzhsLWFBNkxjYUFEUS4iLCJzY3AiOiJ1c2VyX2ltcGVyc29uYXRpb24iLCJzdWIiOiJHQUtTYVRjbUlaVTVodEFaUkZFYXhjUlpqRXFjdVpleXlCdGVSNUVIbVdJIiwidGVuYW50X2N0cnkiOiJVUyIsInRlbmFudF9yZWdpb25fc2NvcGUiOiJXVyIsInRpZCI6IjcyZjk4OGJmLTg2ZjEtNDFhZi05MWFiLTJkN2NkMDExZGI0NyIsInVuaXF1ZV9uYW1lIjoiaXpsaXNib25AbWljcm9zb2Z0LmNvbSIsInVwbiI6Iml6bGlzYm9uQG1pY3Jvc29mdC5jb20iLCJ1dGkiOiJTelNwdWhlYnBVdUdBNDNlV1ZSc0FBIiwidmVyIjoiMS4wIiwieG1zX3BkbCI6Ik5BTSJ9.rNnYpgJijncaqU0aNWD3hjIepali3vmAS8yEOzQxO_z_ynsONmyFfikJiS28EOKw3ejTbCwnecR4Qd8XlmlnShx9NEWnbt_pFEEr2rMoyYxQux7LHmeoTc-opR8H_NVlScmFWwGeXJ3HWKAtoJRJJxZCrNz5M379v8-6spJ1m70wpe8GHqXKnQ5wjp8EhvGFk-hnm7EHUS1nKUznhrGRWA-u08R20WKwrQsMGJrbxoodBMQKYRmyxwhJ4EbPKnM2NVaDmV0D5nwH1gt7ThI0XN3AAkK_hk9PfhTtHkSpnPvlr0S57RSVaP2n3geIPlhmGB2VAUEeUseqnNG10tuQZA",
-                    "scope": "query"
+      const postToken = (iframe, accessToken, scope) => {
+          iframe.contentWindow.postMessage({
+            "type": "postToken",
+            "message": accessToken,
+            "scope": scope
+        }, '*');
+      }
+      const handleIframeMessage = (event) => {
+        console.log("event.data.type: " + event.data.type);
+
+        const scope = event.data.scope === 'query' ? "https://kwetest.eastus.kusto.windows.net/.default" : event.data.scope;
+        const iframe = iframeRef.current;
+        if (event.data.type === "getToken" && iframe && iframe.contentWindow) {
+          if (accounts.length > 0 && inProgress === InteractionStatus.None) {
+            instance.acquireTokenSilent({
+              scopes: [scope],
+              account: accounts[0],
+            })
+            .then((response) => postToken(iframe, response.accessToken, event.data.scope))                
+            .catch((error) => {
+              if (error instanceof InteractionRequiredAuthError) {
+                instance.acquireTokenPopup({ scopes: [scope]})
+                .then((response) => postToken(iframe, response.accessToken, event.data.scope))
+                .catch((error) => {
+                  console.log(`Error acquiring token: ${error}`);
                 });
-            }
-        };
-    
-        // Attach event listener when the component mounts
-        window.addEventListener('message', handleIframeMessage);
-    
-        // Clean up the event listener when the component unmounts
-        return () => {
-          window.removeEventListener('message', handleIframeMessage);
-        };
-      }, []); 
+              } else {
+                console.log(`Error acquiring token: ${error}`);
+              }
+            });
+          }
+        }
+      };
+  
+      // Attach event listener when the component mounts
+      window.addEventListener('message', handleIframeMessage);
+  
+      // Clean up the event listener when the component unmounts
+      return () => {
+        window.removeEventListener('message', handleIframeMessage);
+      };
+
+    }, [iframeRef, accounts, inProgress, instance]);
 
   return (
-    <iframe ref={iframeRef} title="adx example" src='https://dataexplorer.azure.com?f-IFrameAuth=true&workspace=kwe-embed-demo' />
+    <iframe ref={iframeRef} title="adx example" src={`https://dataexplorer.azure.com/clusters/${iframeConfig.cluster}/databases/${iframeConfig.database}?${iframeConfig.features}&workspace=${iframeConfig.workspaceName}`} />
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { App } from './app';
+import { MsalProvider } from '@azure/msal-react';
+import { AuthComponent, msalInstance } from './auth'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MsalProvider instance={msalInstance}>
+      <AuthComponent />
+    </MsalProvider>
   </React.StrictMode>
 );
