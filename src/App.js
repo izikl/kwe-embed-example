fd19e5d0bb91f777c7d8469c5aec6030a6370725
Issac Lisbon <izlisbon@microsoft.com>
2023-05-26 15:02:01 -0700

    add readme and minor refactoring

--- src/App.js
+++ src/App.js
@@ -1,81 +1,104 @@
-import React, { useRef, useEffect } from 'react';
-import { useMsal } from '@azure/msal-react';
-import { InteractionStatus, InteractionRequiredAuthError } from '@azure/msal-browser';
-import './app.css';
+import React, { useRef, useEffect } from "react";
+import { useMsal } from "@azure/msal-react";
+import {
+  InteractionStatus,
+  InteractionRequiredAuthError,
+} from "@azure/msal-browser";
+import "./app.css";
 
 const iframeConfig = {
   cluster: "kwetest.eastus",
   database: "covid",
-  features: "f-IFrameAuth=true",
-  workspaceName: "kwe-embed-demo"
+  features: "f-IFrameAuth=true&f-UseMeControl=false",
+  workspaceName: "kwe-embed-demo",
+};
+
+/**
+ * Map scopes from getToken message to AAD scopes:
+ *   ADX Cluster:       'https://kwetest.eastus.kusto.windows.net/.default',
+ *   Dashboard Service: 'https://rtd-metadata.azurewebsites.net/user_impersonation'
+ *   Graph:             'User.ReadBasic.All',
+ *                      'Group.Read.All'
+ *                      'People.Read'
+ */
+function mapScope(scope) {
+  switch (scope) {
+    case "query":
+      return ["https://kwetest.eastus.kusto.windows.net/.default"];
+    case "People.Read":
+      return ["People.Read", "User.ReadBasic.All", "Group.Read.All"];
+    default:
+      return [scope];
+  }
 }
 
+/**
+ * Post a postToken message
+ */
+const postToken = (iframe, accessToken, scope) => {
+  iframe.contentWindow.postMessage(
+    {
+      type: "postToken",
+      message: accessToken,
+      scope: scope,
+    },
+    "*"
+  );
+};
+
 export function App() {
-    const iframeRef = useRef(null);
-    const { instance, inProgress, accounts } = useMsal();
-    
-    useEffect(() => {
-      const postToken = (iframe, accessToken, scope) => {
-          iframe.contentWindow.postMessage({
-            "type": "postToken",
-            "message": accessToken,
-            "scope": scope
-        }, '*');
-      }
-      const handleIframeMessage = (event) => {
-        console.log("event.data.type: " + event.data.type);
+  const iframeRef = useRef(null);
+  const { instance, inProgress, accounts } = useMsal();
 
-        // used scopes:
-        //   'People.Read',
-        //   'https://kwetest.eastus.kusto.windows.net/.default',
-        //   'https://rtd-metadata.azurewebsites.net/user_impersonation'
-        //   'User.ReadBasic.All',
-        //   'Group.Read.All'
+  useEffect(() => {
+    const handleIframeMessage = (event) => {
+      console.log("event.data.type: " + event.data.type);
 
-        // TODO: this must be approved in ADX 
-        var scopes = [event.data.scope === 'query' ? "https://kwetest.eastus.kusto.windows.net/.default" : event.data.scope];
-        if (scopes[0] === 'People.Read') {
-          scopes = [
-            'People.Read',
-            'User.ReadBasic.All',
-            'Group.Read.All'
-          ]
-        }
+      const aadScopes = mapScope(event.data.scope);
 
-        const iframe = iframeRef.current;
-        if (event.data.type === "getToken" && iframe && iframe.contentWindow) {
-          if (accounts.length > 0 && inProgress === InteractionStatus.None) {
-            instance.acquireTokenSilent({
-              scopes: scopes,
-              account: accounts[0]
+      const iframe = iframeRef.current;
+      if (event.data.type === "getToken" && iframe && iframe.contentWindow) {
+        if (accounts.length > 0 && inProgress === InteractionStatus.None) {
+          instance
+            .acquireTokenSilent({
+              scopes: aadScopes,
+              account: accounts[0],
             })
-            .then((response) => postToken(iframe, response.accessToken, event.data.scope))                
+            .then((response) =>
+              postToken(iframe, response.accessToken, event.data.scope)
+            )
             .catch((error) => {
               if (error instanceof InteractionRequiredAuthError) {
-                instance.acquireTokenPopup({ scopes: scopes})
-                .then((response) => postToken(iframe, response.accessToken, event.data.scope))
-                .catch((error) => {
-                  console.log(`Error acquiring token: ${error}`);
-                });
+                instance
+                  .acquireTokenPopup({ scopes: aadScopes })
+                  .then((response) =>
+                    postToken(iframe, response.accessToken, event.data.scope)
+                  )
+                  .catch((error) => {
+                    console.log(`Error acquiring token: ${error}`);
+                  });
               } else {
                 console.log(`Error acquiring token: ${error}`);
               }
             });
-          }
         }
-      };
-  
-      // Attach event listener when the component mounts
-      window.addEventListener('message', handleIframeMessage);
-  
-      // Clean up the event listener when the component unmounts
-      return () => {
-        window.removeEventListener('message', handleIframeMessage);
-      };
+      }
+    };
 
-    }, [iframeRef, accounts, inProgress, instance]);
+    // Attach event listener when the component mounts
+    window.addEventListener("message", handleIframeMessage);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("message", handleIframeMessage);
+    };
+  }, [iframeRef, accounts, inProgress, instance]);
 
   return (
-    <iframe ref={iframeRef} title="adx example" src={`https://dataexplorer.azure.com/clusters/${iframeConfig.cluster}/databases/${iframeConfig.database}?${iframeConfig.features}&workspace=${iframeConfig.workspaceName}`} />
+    <iframe
+      ref={iframeRef}
+      title="adx example"
+      src={`https://dataexplorer.azure.com/clusters/${iframeConfig.cluster}/databases/${iframeConfig.database}?${iframeConfig.features}&workspace=${iframeConfig.workspaceName}`}
+    />
   );
 }
--- src/auth.js
+++ src/auth.js
@@ -8,14 +8,14 @@ import './auth.css'
 // 1. Redirect URI should be eligible for the Authorization Code Flow with PKCE (https://portal.azure.com/#view/Microsoft_AAD_RegisteredApps/ApplicationMenuBlade/~/Authentication/appId/0289b3ad-4264-4a6c-8f0c-2c56d8177831/isMSAApp~/false)
 // 2. App should Grant Admin permission to Azure Date Explorer -> user_impersonation (https://portal.azure.com/#view/Microsoft_AAD_RegisteredApps/ApplicationMenuBlade/~/CallAnAPI/appId/0289b3ad-4264-4a6c-8f0c-2c56d8177831/isMSAApp~/false) 
 
-const catsAndDogsTenantId = 'd48a02e7-63fd-4c86-960a-4d76b2e9d751';
-const azureDataExplorerTestAppId = '0289b3ad-4264-4a6c-8f0c-2c56d8177831'; // https://portal.azure.com/#view/Microsoft_AAD_RegisteredApps/ApplicationMenuBlade/~/Overview/appId/0289b3ad-4264-4a6c-8f0c-2c56d8177831/isMSAApp~/false
+const tenantId = 'd48a02e7-63fd-4c86-960a-4d76b2e9d751';
+const appId = '0289b3ad-4264-4a6c-8f0c-2c56d8177831'; // https://portal.azure.com/#view/Microsoft_AAD_RegisteredApps/ApplicationMenuBlade/~/Overview/appId/0289b3ad-4264-4a6c-8f0c-2c56d8177831/isMSAApp~/false
 const redirectUri = `${window.location.protocol}//${window.location.host}/blank.html`;
 
 const msalConfig = {
   auth: {
-    clientId: azureDataExplorerTestAppId, 
-    authority: `https://login.microsoftonline.com/${catsAndDogsTenantId}`,
+    clientId: appId, 
+    authority: `https://login.microsoftonline.com/${tenantId}`,
     redirectUri: redirectUri,
   },
 };
